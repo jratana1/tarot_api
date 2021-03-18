cf1761cd13cd66bf826b64578790a11de5cb4590
John Ratana <jratana1@gmail.com>
2021-03-18 01:01:27 -0400

    before action set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,25 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
-  end
+  skip_before_action :authorized, only: [:create]
 
+    @users = User.all
+      render json: @users
+    end
+
+    
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        @token = encode_token(user_id: @user.id)
+        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      else
+        render json: { error: 'failed to create user' }, status: :not_acceptable
+      end
+    end
   
+    private
+  
+    def user_params
+      params.require(:user).permit(:username, :password, :bio, :avatar)
+    end
+  end 
 end
