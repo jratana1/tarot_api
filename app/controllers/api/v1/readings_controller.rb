ef3421dfde7f4b54cb73d619d0ad493a5ab6fe3c
John Ratana <jratana1@gmail.com>
2021-03-19 09:23:24 -0400

    done

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::ReadingsController < ApplicationController
         readings = Reading.all
         render json: ReadingSerializer.new(readings)
     end
+
+    def create
+        reading = Reading.new(:user_id => 1, :question => params[:question])
+
+        reading.cards << Card.find(params[:cards][0])
+        reading.cards << Card.find(params[:cards][1])
+        reading.cards << Card.find(params[:cards][2])
+        if reading.save
+            render json: ReadingSerializer.new(reading)
+        end
+    end
 end
