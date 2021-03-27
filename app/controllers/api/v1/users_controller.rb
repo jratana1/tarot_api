e988cb58d8b04291139614bdb58fc60b2a52540a
John Ratana <jratana1@gmail.com>
2021-03-27 17:16:00 -0400

    updated user control to create or find user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,14 +11,27 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-  
-    if @user.valid?
-      @token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-    else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
-    end
+   
+
+      @user = User.find_by(username: user_params[:username])
+      if @user
+        #User#authenticate comes from BCrypt
+        if @user && @user.authenticate(user_params[:password])
+          # encode token comes from ApplicationController
+          token = encode_token({ user_id: @user.id })
+          render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+          render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+      else
+        @user = User.create(user_params)
+        if @user.valid?
+          @token = encode_token({ user_id: @user.id })
+          render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+          render json: { error: 'failed to create user' }, status: :not_acceptable
+        end
+      end
   end
 
   private
