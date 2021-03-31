502d1f82ea5583e697962823cfb5469b5cd9ad63
John Ratana <jratana1@gmail.com>
2021-03-31 17:22:07 -0400

    updated reading index

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ReadingsController < ApplicationController
     def index
-        readings = Reading.all
+        readings = current_user.readings.last(5)
         render json: ReadingSerializer.new(readings)
     end
 
