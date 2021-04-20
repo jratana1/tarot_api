109ed9ddf707d7e33feaa5732643e95c3e5389f6
John Ratana <jratana1@gmail.com>
2021-04-19 20:44:56 -0400

    update

--- app/controllers/api/v1/readings_controller.rb
+++ app/controllers/api/v1/readings_controller.rb
@@ -1,17 +1,19 @@
 class Api::V1::ReadingsController < ApplicationController
     def index
+
         readings = current_user.readings.last(5)
         render json: ReadingSerializer.new(readings)
     end
 
     def create
-    
+   
         reading = Reading.new(:user_id => current_user.id, :question => params[:question])
 
         reading.cards << Card.find(params[:cards][0])
         reading.cards << Card.find(params[:cards][1])
         reading.cards << Card.find(params[:cards][2])
         if reading.save
+      
             readings = current_user.readings.last(5)
             render json: ReadingSerializer.new(readings)
         end
